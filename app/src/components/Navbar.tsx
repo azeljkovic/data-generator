b02d3573af0f5d46095d155b3bae3c0bc260d56c
Aleksandar Zeljkovic <aleksandar.zeljkovic91@gmail.com>
2022-10-29 14:23:48 +0200

    Initial app commit.

---
+++ app/src/components/Navbar.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Navbar() {
+  return (
+    <div className="Navbar">
+      <h1>Welcome to the data generator</h1>
+      <div>Select the data:</div>
+      <ul>
+        <li>First name</li>
+        <li>Last name</li>
+        <li>e-mail</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Navbar;
