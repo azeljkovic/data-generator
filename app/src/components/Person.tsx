5424ffd749fee42dc25f262b503b0fd811967459
Aleksandar Zeljkovic <aleksandar.zeljkovic91@gmail.com>
2022-10-30 19:48:40 +0100

    Added person component.

--- app/src/components/Person.tsx
+++ app/src/components/Person.tsx
@@ -3,13 +3,7 @@ import React from 'react';
 const Person = () => {
   return (
     <div className="Person">
-      <h1>Welcome to the data generator</h1>
-      <div>Select the data type:</div>
-      <ul>
-        <li>People information</li>
-        <li>Date/Time</li>
-        <li>Random data</li>
-      </ul>
+      <h1>xxx</h1>
     </div>
   );
 }
